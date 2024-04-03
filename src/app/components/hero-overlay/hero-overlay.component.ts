07b89ae636613bb14951e53be593475072431a4f
lusciousMa <ringroe34@gmail.com>
2024-04-03 10:55:07 -0400

    More hero room

--- src/app/components/hero-overlay/hero-overlay.component.ts
+++ src/app/components/hero-overlay/hero-overlay.component.ts
@@ -10,12 +10,15 @@ import {
 interface HeroUpgrade {
   id: number;
   level: HeroUpgradeLevel;
+  image: string;
 }
 
 type HeroUpgradeLevel = 0 | 1 | 2 | 3;
 
 interface Hero {
   id: number;
+  points: number;
+  usedPoints: number;
   upgrades: HeroUpgrade[];
 }
 
@@ -48,25 +51,35 @@ export class HeroOverlayComponent implements OnInit {
       {
         id: 1,
         level: 0,
+        image: 'greenUpgrade1.png',
       },
       {
         id: 2,
         level: 0,
+        image: 'greenUpgrade2.png',
       },
       {
         id: 3,
         level: 0,
+        image: 'greenUpgrade3.png',
       },
       {
         id: 4,
         level: 0,
+        image: 'greenUpgrade4.png',
       },
       {
         id: 5,
         level: 0,
+        image: 'greenUpgrade5.png',
       },
     ];
-    const newHero: Hero = { id: 1, upgrades: upgrades };
+    const newHero: Hero = {
+      id: 1,
+      upgrades: upgrades,
+      points: 5,
+      usedPoints: 0,
+    };
     this.heroes.push({ ...newHero, id: 1 });
     this.heroes.push({ ...newHero, id: 2 });
     this.heroes.push({ ...newHero, id: 3 });
@@ -81,21 +94,46 @@ export class HeroOverlayComponent implements OnInit {
   trackById = (index: number, item: HeroUpgrade) => item.id;
 
   upgradeHero(item: HeroUpgrade) {
-    if (this.currentHero && this.currentHero.upgrades) {
+    if (
+      this.currentHero &&
+      this.currentHero.upgrades &&
+      this.currentHero.points > 0
+    ) {
       this.currentHero.upgrades = this.currentHero.upgrades.map((x) => {
         if (item.id === x.id) {
           if (x.level === 0) {
+            this.subtractUpgradePoints();
             return { ...x, level: 1 };
           }
           if (x.level === 1) {
+            this.subtractUpgradePoints();
             return { ...x, level: 2 };
           }
           if (x.level === 2) {
+            this.subtractUpgradePoints();
             return { ...x, level: 3 };
           }
         }
         return x;
       });
+    }
+  }
+
+  resetUpgrades() {
+    if (this.currentHero && this.currentHero.upgrades) {
+      this.currentHero.upgrades = this.currentHero.upgrades.map((x) => {
+        return { ...x, level: 0 };
+      });
+      this.currentHero.points =
+        this.currentHero.points + this.currentHero.usedPoints;
+      this.currentHero.usedPoints = 0;
+    }
+  }
+
+  subtractUpgradePoints() {
+    if (this.currentHero) {
+      this.currentHero.points = this.currentHero.points - 1;
+      this.currentHero.usedPoints++;
     }
   }
 
