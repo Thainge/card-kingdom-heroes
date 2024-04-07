a08e0c1d838fe859853e2ee2251d69be40fd518e
lusciousMa <ringroe34@gmail.com>
2024-04-07 16:02:55 -0400

    added

--- src/app/components/campaign-overlay/campaign-overlay.component.ts
+++ src/app/components/campaign-overlay/campaign-overlay.component.ts
@@ -2,10 +2,12 @@ import { CommonModule } from '@angular/common';
 import {
   CUSTOM_ELEMENTS_SCHEMA,
   Component,
+  ElementRef,
   EventEmitter,
   Input,
   OnInit,
   Output,
+  ViewChild,
 } from '@angular/core';
 import {
   fadeOutOnLeaveAnimation,
@@ -13,6 +15,12 @@ import {
   fadeInUpOnEnterAnimation,
   fadeOutUpOnLeaveAnimation,
 } from 'angular-animations';
+import Swiper from 'swiper';
+
+interface CampaignBox {
+  id: number;
+  image: string;
+}
 
 @Component({
   selector: 'app-campaign-overlay-overlay',
@@ -33,8 +41,42 @@ export class CampaignOverlayComponent implements OnInit {
   open: boolean = false;
   @Input('open') set openChanged(x: boolean) {
     this.open = x;
+    this.currentIndex = 0;
+    setTimeout(() => {
+      this.swiper = this.swiperRef?.nativeElement.swiper;
+    }, 0);
   }
   @Output() onCloseMenu = new EventEmitter<boolean>(false);
+
+  @ViewChild('swiper') swiperRef: ElementRef | undefined;
+  swiper?: Swiper;
+  currentIndex: number = 0;
+  campaigns: CampaignBox[] = [
+    {
+      id: 1,
+      image: 'normalCampaign.png',
+    },
+    {
+      id: 2,
+      image: 'linkCampaign.png',
+    },
+    {
+      id: 3,
+      image: 'marioCampaign.png',
+    },
+    {
+      id: 4,
+      image: 'tf2Campaign.png',
+    },
+    {
+      id: 5,
+      image: 'kirbyCampaign.png',
+    },
+    {
+      id: 6,
+      image: 'donkeyKongCampaign.png',
+    },
+  ];
 
   constructor() {}
 
@@ -44,6 +86,10 @@ export class CampaignOverlayComponent implements OnInit {
 
   closeMenu() {
     this.onCloseMenu.emit(false);
+  }
+
+  onActiveIndexChange() {
+    this.currentIndex = this.swiper?.activeIndex ?? 0;
   }
 
   trackById = (index: number, item: any) => item.id;
